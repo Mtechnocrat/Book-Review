a1c3dc8878148c633dc604fb6f06f1560b6c3d61
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2025-07-23 21:42:56 +0530

    User and Book model added

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,1 +1,20 @@
 // Book schema
+// server/models/Book.js
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    author: { type: String, required: true, trim: true },
+    description: { type: String },
+    coverUrl: String,
+    averageRating: { type: Number, default: 0 },
+    ratingsCount: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
+
+// text index for search
+bookSchema.index({ title: "text", author: "text" });
+
+export default mongoose.model("Book", bookSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,31 @@
-// User schema
+// User schema// server/models/User.js
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: { type: String, required: true, minlength: 6 }
+  },
+  { timestamps: true }
+);
+
+// hash pw before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.comparePassword = function (plain) {
+  return bcrypt.compare(plain, this.password);
+};
+
+export default mongoose.model("User", userSchema);
