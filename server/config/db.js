29a892bdb3cf9b3a56d3d03867e81dcd97abaa89
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2025-07-23 21:40:05 +0530

    db added

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,13 @@
 // MongoDB connection setup
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${mongoose.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
